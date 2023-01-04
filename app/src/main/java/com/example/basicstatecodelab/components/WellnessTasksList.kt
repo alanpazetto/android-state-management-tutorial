a20202e5712d4a0a6c6486d9bc1ef4756ab2d699
Alan Pazetto <alan.pazetto@profusion.mobi>
2023-01-04 12:14:24 -0300

    feat(WellnessTaskList): add compoent

---
+++ app/src/main/java/com/example/basicstatecodelab/components/WellnessTasksList.kt
@@ -0,0 +1,26 @@
+package com.example.basicstatecodelab.components
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.models.WellnessTask
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier,
+        rememberLazyListState(),
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
