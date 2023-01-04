75fba76857974467694623c160ae9e76b81df298
Alan Pazetto <alan.pazetto@profusion.mobi>
2023-01-04 12:36:12 -0300

    refactor(WellnessTasks): add ViewModel to control the state

--- app/src/main/java/com/example/basicstatecodelab/components/WellnessTasksList.kt
+++ app/src/main/java/com/example/basicstatecodelab/components/WellnessTasksList.kt
@@ -4,23 +4,29 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.basicstatecodelab.models.WellnessTask
 
 @Composable
 fun WellnessTasksList(
-    modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(
-        modifier = modifier,
-        rememberLazyListState(),
+        modifier = modifier
     ) {
-        items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+        items(
+            items = list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) }
+            )
         }
     }
 }
-
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
--- app/src/main/java/com/example/basicstatecodelab/screens/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/screens/WellnessScreen.kt
@@ -1,13 +1,27 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.basicstatecodelab.components.StatefulCounter
 import com.example.basicstatecodelab.components.WellnessTasksList
+import com.example.basicstatecodelab.screens.WellnessViewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier = modifier) {
-        StatefulCounter(modifier)
-        WellnessTasksList()
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
     }
 }
