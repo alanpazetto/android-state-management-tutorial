8556dded670ea0c4a07b89d5070def4a41114eed
Alan Pazetto <alan.pazetto@profusion.mobi>
2023-01-04 11:59:47 -0300

    refactor(WaterCounter): update component to state hoist

--- app/src/main/java/com/example/basicstatecodelab/screens/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/screens/WellnessScreen.kt
@@ -1,8 +1,8 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.basicstatecodelab.components.WaterCounter
+import com.example.basicstatecodelab.components.StatefulCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
